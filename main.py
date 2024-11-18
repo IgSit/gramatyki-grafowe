b4f77e74522ba5a6cc6967c8d0e79b6509574586
Demolen22 <DemuChannel@gmail.com>
2024-11-18 11:48:59 +0100

    setup draft, nx test

---
+++ main.py
@@ -0,0 +1,38 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class HyperGraph:
+    def __init__(self, nodes: list[int], edges: set[int]):
+        self.nodes = nodes
+        self.edges = edges
+        assert self._check_data(), "Inconsistent hypergraph parametrs"
+        self.nx_graph = self._construct_nx_graph()
+        
+    def _check_data(self) -> bool:
+        pass
+
+    def _construct_nx_graph(self) -> nx.Graph:
+        pass
+
+    def visualize(self):
+        nx.draw(self.nx_graph)
+        plt.show()
+
+class Production:
+    def __init__(self, left_side_hypergraph: HyperGraph):
+        self.left_side_hypergraph = left_side_hypergraph
+
+    def check() -> bool:
+        pass
+    
+    def apply() -> HyperGraph:
+        pass
+
+if __name__ == "__main__":
+    # nx test
+    graph = nx.Graph()
+    graph.add_nodes_from([('v1', {'pos':(0, 0)}),('v2', {'pos':(4, 0)}),('v3', {'pos':(4, 4)}),('v4', {'pos':(0, 4)}),('q', {'pos':(2, 2)})])
+    graph.add_edges_from([('v1','v2'),('v2','v3'),('v3', 'v4'), ('v4','v1')])
+    graph.add_edges_from([('v1','q'),('v2','q'),('v3', 'q'), ('v4', 'q')])
+    nx.draw(graph, nx.get_node_attributes(graph, 'pos'), with_labels=True)
+    plt.show()
