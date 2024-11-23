75231c0f7a5a2a5493d13224c293e146c2481954
Demolen22 <DemuChannel@gmail.com>
2024-11-23 21:35:09 +0100

    fully working p1

--- productions/p1.py
+++ productions/p1.py
@@ -32,15 +32,26 @@ class P1(Production):
     def apply(self, graph: HyperGraph, hyper_node: str) -> HyperGraph:
         neighbours = tuple(graph.get_neighbours(hyper_node))
         subgraph_edges = tuple(nx.subgraph(graph.nx_graph, neighbours).edges)
+        new_nodes_pos = [(edge, graph.calculate_mean_node_position(edge)) for edge in subgraph_edges]
+        middle_node_pos = graph.calculate_mean_node_position(neighbours)
+        new_nodes_ids = list(map(lambda x: f'a{x[1]}', new_nodes_pos))
+        middle_node_id = 'a' + str(middle_node_pos)
+        helper_dict = {n:{n, middle_node_id} for n in neighbours}
+        for i, (edge, _) in enumerate(new_nodes_pos):
+            for v in edge:
+                helper_dict[v].add(new_nodes_ids[i])
+        inner_edges = [({new_node, middle_node_id}, {'label': 'E', 'B': False}) for new_node in new_nodes_ids]
+        outer_edges = [({v, new_nodes_ids[i]}, {'label': 'E', 'B': graph.nx_graph.edges[*edge]['B']}) for i, edge in enumerate(subgraph_edges) for v in edge]
+        hyper_edges = list(map(lambda x: (x, {'label': 'Q', 'R': False}), helper_dict.values()))
+        new_nodes = list(zip(new_nodes_ids, map(lambda x: {'pos':x[1], 'h':graph.nx_graph.edges[*x[0]]['B']}, new_nodes_pos))) + [(middle_node_id, {'pos':middle_node_pos, 'h':False})]
+        
         graph.shrink(nodes=[], edges=[
             set(neighbours), # remove hyperedge
             *(set(edge) for edge in subgraph_edges) # remove other edges
         ])
-        new_nodes_pos = [graph.calculate_mean_node_position(edge) for edge in subgraph_edges] + [graph.calculate_mean_node_position(neighbours)]
-        new_nodes = list(map(lambda x: (f'a{x}', {'pos':x, 'h':False}), new_nodes_pos))
         graph.extend(
-            nodes=new_nodes, 
-            edges=[] # TODO: create new edges
+            nodes=new_nodes,
+            edges=inner_edges + outer_edges + hyper_edges
         )
         return graph
 
