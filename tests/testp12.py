6df4fb52df737acd07f931e143cef56c81cdea9a
kwdrt <jakubkcichocki@gmail.com>
2024-11-24 19:29:21 +0100

    more tests for p12

--- tests/testp12.py
+++ tests/testp12.py
@@ -93,3 +93,51 @@ class TestP12():
                     hyper_graph2 = production.apply(hyper_graph2, hyper_node)
                     hyper_graph2.visualize()
                     break
+
+        hyper_graph3 = HyperGraph(
+            nodes=[
+                ('v1', {'pos': (2, -2), 'h': False}),
+                ('v2', {'pos': (4, -2), 'h': False}),
+                ('v3', {'pos': (4, 2), 'h': False}),
+                ('v4', {'pos': (2, 2), 'h': False}),
+                ('v5', {'pos': (6, 0), 'h': False}),
+                ('v6', {'pos': (0, 0), 'h': False}),
+                ('v7', {'pos': (3, -2), 'h': True}),
+                ('v9', {"pos": (0, -6), 'h': False}),
+                ('v10', {"pos": (0, 6), 'h': False}),
+                ('v11', {"pos": (6, -6), 'h': False}),
+                ('v12', {"pos": (6, 6), 'h': False}),
+                ('v8', {'pos': (1, 1), 'h': True}),
+            ],
+            edges=[
+                ({'v1', 'v7'}, {'label': 'E', 'B': False}),
+                ({'v7', 'v2'}, {'label': 'E', 'B': False}),
+                ({'v2', 'v5'}, {'label': 'E', 'B': False}),
+                ({'v5', 'v3'}, {'label': 'E', 'B': False}),
+                ({'v3', 'v4'}, {'label': 'E', 'B': False}),
+                ({'v6', 'v1'}, {'label': 'E', 'B': False}),
+                ({'v4', 'v8'}, {'label': 'E', 'B': False}),
+                ({'v8', 'v6'}, {'label': 'E', 'B': False}),
+                ({'v6', 'v10'}, {'label': 'E', 'B': False}),
+                ({'v6', 'v9'}, {'label': 'E', 'B': False}),
+                ({'v9', 'v1'}, {'label': 'E', 'B': False}),
+                ({'v2', 'v11'}, {'label': 'E', 'B': False}),
+                ({'v11', 'v5'}, {'label': 'E', 'B': False}),
+                ({'v5', 'v12'}, {'label': 'E', 'B': False}),
+                ({'v3', 'v12'}, {'label': 'E', 'B': False}),
+                ({'v10', 'v4'}, {'label': 'E', 'B': False}),
+                ({'v1', 'v2', 'v5', 'v3', 'v4', 'v6'}, {'label': 'S', 'R': True})
+            ]
+        )
+
+        productions = [P12()]
+
+        hyper_graph3.visualize()
+
+        for production in productions:
+            for hyper_node in hyper_graph3.hyper_nodes:
+                if production.check(hyper_graph3, hyper_node):
+                    hyper_graph3.visualize()
+                    hyper_graph3 = production.apply(hyper_graph3, hyper_node)
+                    hyper_graph3.visualize()
+                    break
