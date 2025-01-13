b83c6f1e84f4c72ddf7cf086721d0fff2a8908bc
Piotr Ryczek <piotrryczek@student.agh.edu.pl>
2025-01-13 06:24:24 +0100

    G3 started working on g3 reasoning

---
+++ reasonings/g3.py
@@ -0,0 +1,64 @@
+from fontTools.misc.cython import returns
+from matplotlib import pyplot as plt
+
+from graph.hypergraph import HyperGraph
+from productions import *
+
+if __name__ == "__main__":
+    starting_graph = HyperGraph(
+        nodes=[
+            ("v1", {"pos": (-4, 0), "h": False}),
+            ("v2", {"pos": (-2, 3), "h": False}),
+            ("v3", {"pos": (2, 3), "h": False}),
+            ("v4", {"pos": (4, 0), "h": False}),
+            ("v5", {"pos": (2, -3), "h": False}),
+            ("v6", {"pos": (-2, -3), "h": False}),
+
+            ("v7", {"pos": (-7, -7), "h": False}),
+            ("v8", {"pos": (7, -7), "h": False}),
+            ("v9", {"pos": (7, 7), "h": False}),
+            ("v10", {"pos": (-7, 7), "h": False}),
+            ("v11", {"pos": (-7, 0), "h": False}),
+            ("v12", {"pos": (7, 0), "h": False}),
+        ],
+        edges=[
+            ({"v1", "v2"}, {"label": "E", "B": False}),
+            ({"v2", "v3"}, {"label": "E", "B": False}),
+            ({"v3", "v4"}, {"label": "E", "B": False}),
+            ({"v4", "v5"}, {"label": "E", "B": False}),
+            ({"v5", "v6"}, {"label": "E", "B": False}),
+            ({"v6", "v1"}, {"label": "E", "B": False}),
+            ({"v1", "v2", "v3", "v4", "v5", "v6"}, {"label": "S", "R": False}),
+
+            ({"v10", "v11"}, {"label": "E", "B": True}),
+            ({"v11", "v7"}, {"label": "E", "B": True}),
+            ({"v7", "v8"}, {"label": "E", "B": True}),
+            ({"v8", "v12"}, {"label": "E", "B": True}),
+            ({"v12", "v9"}, {"label": "E", "B": True}),
+            ({"v9", "v10"}, {"label": "E", "B": True}),
+
+            ({"v2", "v10"}, {"label": "E", "B": False}),
+            ({"v1", "v11"}, {"label": "E", "B": False}),
+            ({"v6", "v7"}, {"label": "E", "B": False}),
+            ({"v5", "v8"}, {"label": "E", "B": False}),
+            ({"v4", "v12"}, {"label": "E", "B": False}),
+            ({"v3", "v9"}, {"label": "E", "B": False}),
+
+            ({"v1", "v2", "v10", "v11"}, {"label": "Q", "R": False}),
+            ({"v1", "v6", "v7", "v11"}, {"label": "Q", "R": False}),
+            ({"v5", "v8", "v7", "v6"}, {"label": "Q", "R": False}),
+            ({"v12", "v4", "v5", "v8"}, {"label": "Q", "R": False}),
+            ({"v4", "v3", "v9", "v12"}, {"label": "Q", "R": False}),
+            ({"v2", "v3", "v9", "v10"}, {"label": "Q", "R": False}),
+            ],
+    )
+
+    starting_graph.visualize()
+
+    prod_list = [P7(), P1(), P7(), P8(), P22(), P2(), P11(), P1(), P7(), P8(), P8(), P2(), P3(), P1()]
+
+    for index, production in enumerate(prod_list):
+        for hyper_node in starting_graph.hyper_nodes:
+            if production.check(starting_graph, hyper_node):
+                hyper_graph = production.apply(starting_graph, hyper_node)
+                hyper_graph.visualize()
